0b2ef28a47818c697f7e1e98e5af3ad1cf667119
Rizwankhane <rizwan@cedcoss.com>
2022-02-05 11:21:22 +0530

    task8

--- src/scriptfile.js
+++ src/scriptfile.js
@@ -1,8 +1,7 @@
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
 
-var a=5;
-var b=10;
-var c = "I am a";
-
-a = a + 1;
-b = b + 5;
-c = c + " String!";
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
