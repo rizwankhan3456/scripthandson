870b36e5b2908bef83dbe21cd41c1038b0ede375
Rizwankhane <rizwan@cedcoss.com>
2022-02-05 15:29:55 +0530

    task58

--- src/scriptfile.js
+++ src/scriptfile.js
@@ -1,16 +1,5 @@
-function nextInLine(arr, item) {
+function welcomeToBooleans() {
 
-    var queue = arr.push(item);
-  
-    var removeItem = arr.shift();
-  
-    return removeItem;  
-  }
-  
-  
-  var testArr = [1,2,3,4,5];
-  
-  
-  console.log("Before: " + JSON.stringify(testArr));
-  console.log(nextInLine(testArr, 6));  
-  console.log("After: " + JSON.stringify(testArr));
+    return true; 
+    
+    }
