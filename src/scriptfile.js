f90c4d7b0d5227f90d7a2310361a2eff9103fcad
Rizwankhane <rizwan@cedcoss.com>
2022-02-05 11:23:15 +0530

    task9

--- src/scriptfile.js
+++ src/scriptfile.js
@@ -1,7 +1,2 @@
-var studlyCapVar;
-var properCamelCase;
-var titleCaseOver;
-
-studlyCapVar = 10;
-properCamelCase = "A String";
-titleCaseOver = 9000;
+let catName = "Oliver";
+let catSound = "Meow!";
