2c4a51268dd495aa2a84726bac8d6d9a3e8038e0
Rizwankhane <rizwan@cedcoss.com>
2022-02-05 14:23:44 +0530

    task48

--- src/scriptfile.js
+++ src/scriptfile.js
@@ -1,3 +1,1 @@
-const myArray = [["John", 23], ["dog", 3]];
-myArray.shift();
-myArray.unshift( ["Paul", 35] );
+const myList = [["Chocolate Bar", 15],["Chocolate Bar", 15],["Chocolate Bar", 15],["Chocolate Bar", 15],["Chocolate Bar", 15]];
